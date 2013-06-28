60d19862277927a56659d35e046db6573552f4e8
Robin Nilsson <snokstyle@gmail.com>
2013-06-28 22:11:39 +0200

    Add Pins Associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,7 @@ class Pin < ActiveRecord::Base
   attr_accessible :description
 
   validates :description, presence: true, length: { :minimum => 3 }
+
+  belongs_to :user
+  validates :user_id, presence: true
 end
